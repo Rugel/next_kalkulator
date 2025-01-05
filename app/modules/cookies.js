6c3c3f52af8814ee92789c44d7a7e545f71ac843
gdychala <g.dychala@gmail.com>
2025-01-05 19:01:00 +0100

    fixed cookies debug

--- app/modules/cookies.js
+++ app/modules/cookies.js
@@ -1,6 +1,6 @@
 import CookieConsent, { Cookies } from "react-cookie-consent";
 import Link from "next/link";
 
-const Cookie = () => <div><CookieConsent debug={true} buttonText="Rozumiem" style={{ background: "#2B373B" }} buttonStyle={{ color: "black", fontSize: "13px", backgroundColor: "white" }}>Ta strona używa plików cookies, aby zapewnić najlepsze doświadczenie użytkownika. Kontynuując korzystanie ze strony, wyrażasz na to zgodę - <Link style={{color:'orange'}} href="/cookies_info">więcej informacji.</Link></CookieConsent></div>;
+const Cookie = () => <div><CookieConsent buttonText="Rozumiem" style={{ background: "#2B373B" }} buttonStyle={{ color: "black", fontSize: "13px", backgroundColor: "white" }}>Ta strona używa plików cookies, aby zapewnić najlepsze doświadczenie użytkownika. Kontynuując korzystanie ze strony, wyrażasz na to zgodę - <Link style={{color:'orange'}} href="/cookies_info">więcej informacji.</Link></CookieConsent></div>;
 
 export default Cookie;
