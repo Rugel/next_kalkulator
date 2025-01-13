8bf2ccc16caa7ea6e79e9408017090ffa019a545
gdychala <g.dychala@gmail.com>
2025-01-13 22:44:20 +0100

    modificated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
+///** @type {import('next').NextConfig} */
+// nextConfig = {
+//    output: 'export',
+//};
+
+//export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-};
-
-export default nextConfig;
+    output: 'export', // Statyczne eksportowanie aplikacji
+    reactStrictMode: true, // Włącza tryb ścisły Reacta dla łatwiejszego debugowania
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        // Rozwiązuje problemy z niektórymi bibliotekami w przeglądarce
+        config.resolve.fallback = { fs: false };
+      }
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
