685d34b59e82f75799d31bd9181ef6ea9efbfd77
gdychala <g.dychala@gmail.com>
2025-02-21 22:43:05 +0100

    fixed layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+/*import "./globals.css";
 import Script from 'next/script';
 import AnalyticsListener from './modules/analytics-listener';
 
@@ -36,6 +36,88 @@ export default function RootLayout({
     <html lang="pl">
       <head>
       <meta name="google-site-verification" content="wWDCgkmW374nhFic4ZNADQdsdDPPJq4hrp3cdf81KXw" />
+        {/* Global Site Tag 
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-PHKBZYQPHS`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PHKBZYQPHS', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+        <meta name="apple-mobile-web-app-title" content="Kalkulator Wynagrodzeń" />
+        {/* <link rel="canonical" href="https://stawka-godzinowa.pl/" />*
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              name: "Kalkulator Wynagrodzeń",
+              url: "https://stawka-godzinowa.pl",
+              logo: "https://stawka-godzinowa.pl/logo.png",
+            }),
+          }}
+        />
+
+      </head>
+      <body>
+        <AnalyticsListener />
+        {children}
+      </body>
+    </html>
+  );
+}*/
+
+
+import "./globals.css";
+import Script from 'next/script';
+import AnalyticsListener from './modules/analytics-listener';
+
+export const metadata = {
+  metadataBase: new URL('https://stawka-godzinowa.pl'),
+  alternates: {
+    canonical: '/',
+  },
+  title: "Kalkulator wynagrodzeń dla pracownika ze stawką godzinową",
+  description: "Kalkulator wynagrodzeń 2025 obliczy kwotę netto lub brutto na podstawie godzin pracy, stawki godzinowej, świadczenia chorobowego, urlopu oraz dodatków",
+  openGraph: {
+    title: "Stawka godzinowa? - ten Kalkulator obliczy Twoje wynagrodzenie!",
+    description: "Kalkulator wynagrodzeń 2025 obliczy kwotę netto lub brutto na podstawie godzin pracy, stawki godzinowej, świadczenia chorobowego, urlopu oraz dodatków",
+    url: "https://stawka-godzinowa.pl",
+    siteName: "Kalkulator Wynagrodzeń",
+    images: [
+      {
+        url: "https://stawka-godzinowa.pl/image.png",
+        width: 1200,
+        height: 630,
+        alt: "Kalkulator Wynagrodzeń",
+      },
+    ],
+    locale: "pl_PL",
+    type: "website",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pl">
+      <head>
+        <meta name="google-site-verification" content="wWDCgkmW374nhFic4ZNADQdsdDPPJq4hrp3cdf81KXw" />
         {/* Global Site Tag */}
         <Script
           strategy="afterInteractive"
@@ -56,20 +138,32 @@ export default function RootLayout({
           }}
         />
         <meta name="apple-mobile-web-app-title" content="Kalkulator Wynagrodzeń" />
-        {/* <link rel="canonical" href="https://stawka-godzinowa.pl/" />*/}
+        {/* Dane strukturalne dla SoftwareApplication */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
               "@context": "https://schema.org",
-              "@type": "WebSite",
-              name: "Kalkulator Wynagrodzeń",
-              url: "https://stawka-godzinowa.pl",
-              logo: "https://stawka-godzinowa.pl/logo.png",
+              "@type": "SoftwareApplication",
+              "name": "Kalkulator Wynagrodzeń",
+              "description": "Kalkulator wynagrodzeń 2025 obliczy kwotę netto lub brutto na podstawie godzin pracy, stawki godzinowej, świadczenia chorobowego, urlopu oraz dodatków",
+              "url": "https://stawka-godzinowa.pl",
+              "image": "https://stawka-godzinowa.pl/image.png",
+              "applicationCategory": "Finance",
+              "operatingSystem": "Web",
+              "offers": {
+                "@type": "Offer",
+                "price": "0",
+                "priceCurrency": "PLN",
+              },
+              "publisher": {
+                "@type": "Organization",
+                "name": "Stawka Godzinowa",
+                "logo": "https://stawka-godzinowa.pl/logo.png",
+              },
             }),
           }}
         />
-
       </head>
       <body>
         <AnalyticsListener />
