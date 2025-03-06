2f58dfa7834932cf78e9037ff795f1201f21f351
gdychala <g.dychala@gmail.com>
2025-03-06 19:34:44 +0100

    recommit

---
+++ app/modules/AdSenseInArticle.js
@@ -0,0 +1,26 @@
+import Script from 'next/script';
+
+const AdSenseInArticle = () => {
+  return (
+    <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXXXX"
+        strategy="afterInteractive" // Ładuje skrypt po interaktywności strony
+      />
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block', textAlign: 'center' }}
+        data-ad-format="fluid"
+        data-ad-layout="in-article"
+        data-ad-client="ca-pub-8789064360135564"
+        data-ad-slot="5751543216"
+      />
+      <Script id="adsense-init" strategy="afterInteractive">
+        {`(adsbygoogle = window.adsbygoogle || []).push({});`}
+      </Script>
+    </>
+  );
+};
+
+export default AdSenseInArticle;
