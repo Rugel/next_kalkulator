02cfad0138f37eeacca92ddac5b5110e355f46c1
gdychala <g.dychala@gmail.com>
2026-01-06 19:37:56 +0100

    updated sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -29,5 +29,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: 'monthly',
             priority: 0.8,
         },
+        {
+            url: `${baseUrl}/kalkulator-umowy-zlecenie`,
+            lastModified: currentDate,
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: `${baseUrl}/kalkulator-b2b`,
+            lastModified: currentDate,
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
     ]
 }
