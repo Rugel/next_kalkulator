b1224fc8e68d4642cf9553a022936765f92db69f
gdychala <g.dychala@gmail.com>
2026-01-17 20:21:51 +0100

    UTF-8

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,6 +69,7 @@ export default function RootLayout({
   return (
     <html lang="pl">
       <head>
+        <meta charSet="UTF-8" />
         <meta name="google-site-verification" content="wWDCgkmW374nhFic4ZNADQdsdDPPJq4hrp3cdf81KXw" />
         {/* Global Site Tag */}
         <Script
