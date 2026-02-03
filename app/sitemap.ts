0b622667df0cce7bc71b0de4f50803cd688d742e
gdychala <g.dychala@gmail.com>
2026-02-03 11:51:32 +0100

    updated sittemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -41,5 +41,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: 'monthly',
             priority: 0.8,
         },
+        {
+            url: `${baseUrl}/kalkulator-inflacji`,
+            lastModified: currentDate,
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
     ]
 }
